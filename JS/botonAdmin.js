6efdd96ed625c7f3c498ac4985f2cf8e44b97c92
Ignacio Guardo <nachoguardoruiz@gmail.com>
2023-04-25 19:51:55 -0300

    Se agregaron estilos

--- JS/botonAdmin.js
+++ JS/botonAdmin.js
@@ -1,13 +1,16 @@
 document.querySelector('#botonAdmin').addEventListener('click', ()=>{
 
-    const Administradores = JSON.parse(localStorage.getItem('usuariosLogueados'));
-    if(Administradores){
-        if(Administradores.email === 'nachoguardo1@gmail.com' || Administradores.email === 'diegofenino1@gmail.com' || Administradores.email ==='alealaniz1@gmail.com' || Administradores.email === 'lautarocuello1@gmail.com'){
-            window.location.href = '/pages/admin.html'
-        }else{
-            alert('No tienes los permisos para ingresar a esta seccion')
-        }
-    }else{
-        alert('No tienes los permisos para ingresar a esta seccion')
+    const Administradores = JSON.parse(localStorage.getItem('usuariosLogueados'))  || [];
+    if(Administradores.email === 'nachoguardo1@gmail.com' || Administradores.email === 'diegofenino1@gmail.com' || Administradores.email === 'alealaniz1@gmail.com' || Administradores.email === 'lautarocuello1@gmail.com'){
+        window.location.href = '/pages/admin.html'
     }
 })
+
+function mostrarBotonAdmin(){
+    const Administradores = JSON.parse(localStorage.getItem('usuariosLogueados'))  || [];
+    if(Administradores.email === 'nachoguardo1@gmail.com'  || Administradores.email === 'diegofenino1@gmail.com' || Administradores.email === 'alealaniz1@gmail.com' || Administradores.email === 'lautarocuello1@gmail.com'){
+        document.querySelector('#botonAdmin').style.display = 'block'
+    }
+}
+
+mostrarBotonAdmin()
